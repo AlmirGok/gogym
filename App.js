b883858428b0fc320894ed6c747dc809f7feaaaf
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2022-12-22 14:32:33 -0300

    Estrutura da aplicação

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoader] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View>
-      <Text>Hello!!</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoader ? <Text>Olá Goker !!</Text> : <View />}
     </View>
   );
 }
