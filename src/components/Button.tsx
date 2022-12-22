e90ff67a1a5a9b915bd492ff89560a93a596e274
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2022-12-22 16:49:02 -0300

    Component Library - Utilizando Variantes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,17 +4,25 @@ type Props = IButtonProps & {
   title: string;
 };
 
-export function Button({ title, ...rest }: Props) {
+export function Button({ title, variant, ...rest }: Props) {
   return (
     <ButtonNativeBase
       w="full"
       h={14}
-      bg="green.700"
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor="green.500"
       rounded="full"
-      _pressed={{ bg: "green.500" }}
+      _pressed={{
+        bg: variant === "outline" ? "gray.500" : "green.500",
+      }}
       {...rest}
     >
-      <Text color="white" fontFamily="heading" fontSize="sm">
+      <Text
+        color={variant === "outline" ? "green.500" : "white"}
+        fontFamily="heading"
+        fontSize="sm"
+      >
         {title}
       </Text>
     </ButtonNativeBase>
