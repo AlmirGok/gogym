ed5c3ea9529dcc7c2afb0f6289a7707fe7374038
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2022-12-22 16:19:52 -0300

    Component Library - Componente de Input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,6 +3,8 @@ import { VStack, Image, Text, Center, Heading } from "native-base";
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 
+import { Input } from "@components/Input";
+
 export function SignIn() {
   return (
     <VStack flex={1} bg="gray.700">
@@ -22,6 +24,8 @@ export function SignIn() {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse sua conta
         </Heading>
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
       </Center>
     </VStack>
   );
