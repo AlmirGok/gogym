2054af9b0f1e145907effa3172397d6e6dd5b956
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2022-12-29 15:10:45 -0300

    Rotas Públicas - Criando o contexto de navegação

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from "react-native";
 import { NativeBaseProvider } from "native-base";
-import { THEME } from "./src/theme";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+
 import { Loading } from "@components/Loading";
-import { SignUp } from "@screens/SignUp";
+
+import { THEME } from "./src/theme";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoader] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoader ? <SignUp /> : <Loading />}
+      {fontsLoader ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
