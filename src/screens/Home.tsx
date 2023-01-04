682b3f367e493371615522b1cd8b791d3db502ad
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-04 17:44:55 -0300

    Finalizando as Interfaces - Header da lista de exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack, FlatList } from "native-base";
+import { VStack, FlatList, HStack, Heading, Text } from "native-base";
 
 export function Home() {
   const [groups, setGroups] = useState([
@@ -32,6 +32,16 @@ export function Home() {
         my={8}
         maxH={10}
       />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
