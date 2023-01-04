b78b3871f32f11fc6719ddf7c3559d6114a96ede
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-04 16:11:34 -0300

    Finalizando as Interfaces - Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,16 @@
 import { Heading, HStack, Text, VStack } from "native-base";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{
+          uri: "https://http2.mlstatic.com/D_NQ_NP_719683-MLA50145427713_052022-O.webp",
+        }}
+        alt="Image do usuário"
+        size={16}
+      />
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,
