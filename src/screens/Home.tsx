19623739c0acdc64c9992cad870470c88b0f9cbe
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-05 10:58:00 -0300

    Finalizando as Interfaces - Recap da Home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -29,7 +29,9 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
             onPress={() => setGroupSelected(item)}
           />
         )}
