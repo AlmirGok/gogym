91c096f31e78804891957c0f09d48e652665854f
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-05 17:28:08 -0300

    Finalizando as Interfaces - Foto do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,23 @@
-import { VStack } from "native-base";
+import { Center, ScrollView, VStack } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{
+              uri: "https://http2.mlstatic.com/D_NQ_NP_719683-MLA50145427713_052022-O.webp",
+            }}
+            alt="Image do usuário"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
