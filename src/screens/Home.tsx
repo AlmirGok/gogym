7b6469cb5efad2c39b0528c491716b20bae95b0f
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-06 10:49:10 -0300

    Finalizando as Interfaces - Navegando para tela de exercício

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { VStack, FlatList, HStack, Heading, Text } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { ExerciseCard } from "@components/ExerciseCard";
@@ -18,6 +22,12 @@ export function Home() {
     "Levantamento terras",
   ]);
   const [groupSelected, setGroupSelected] = useState("costas");
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate("exercite");
+  }
 
   return (
     <VStack flex={1}>
@@ -53,7 +63,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({ item }) => <ExerciseCard />}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}
         />
