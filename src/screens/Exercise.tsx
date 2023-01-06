9b723abcca366ddb1cdf938fc97a1d08af174fb6
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-06 10:56:03 -0300

    Finalizando as Interfaces - Criando botão de voltar

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,25 @@
-import { Center, Text } from "native-base";
+import { VStack, Icon } from "native-base";
+import { TouchableOpacity } from "react-native";
+
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="white">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12}>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={Feather} name="arrow-left" color="green.500" size={6} />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   );
 }
