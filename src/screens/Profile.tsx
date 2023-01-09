cb7e2607d847e114e331b8d66618ae35cb616f2c
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-09 11:58:44 -0300

    Galeria de Fotos - Recuperando foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -22,12 +22,15 @@ export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
 
   async function handleUserPhotoSelect() {
-    await ImagePicker.launchImageLibraryAsync({
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
       aspect: [4, 4],
       allowsEditing: true,
     });
+    if (photoSelected.canceled) {
+      return;
+    }
   }
 
   return (
