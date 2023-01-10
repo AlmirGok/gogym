fd9dadc5b995ef527079b5e1a66052ce2b37f285
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-10 17:19:03 -0300

    Galeria de Fotos - Ativando o carregamento da foto

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -25,16 +25,25 @@ export function Profile() {
   );
 
   async function handleUserPhotoSelect() {
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    });
-    if (photoSelected.canceled) {
-      return;
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+      if (photoSelected.canceled) {
+        return;
+      }
+      if (photoSelected.assets[0].uri) {
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
     }
-    setUserPhoto(photoSelected.assets[0].uri);
   }
 
   return (
