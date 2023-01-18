c1ac0b2de1d4a9028bea6495005142aae6a384c5
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-18 11:33:29 -0300

    Compartilhando estados no contexto

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
+import { createContext, ReactNode, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
-import { createContext, ReactNode } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -14,18 +14,14 @@ export const AuthContext = createContext<AuthContextDataProps>(
 );
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({
+    id: "1",
+    name: "Almir Santos",
+    email: "almirwebdev@gmail.com",
+    avatar: "almir.png",
+  });
+
   return (
-    <AuthContext.Provider
-      value={{
-        user: {
-          id: "1",
-          name: "Almir Santos",
-          email: "almirwebdev@gmail.com",
-          avatar: "almir.png",
-        },
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
   );
 }
