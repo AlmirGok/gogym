33e53548ac69c21a8589d9de263e7d3ad3bb2da1
Almir Santos <103596010+AlmirGok@users.noreply.github.com>
2023-01-19 10:05:58 -0300

    Tratando Erros da API

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,15 +1,19 @@
 import axios from "axios";
 
+import { AppError } from "@utils/AppError";
+
 const api = axios.create({
   baseURL: "http://192.168.0.106:3333",
 });
 
-api.interceptors.request.use(
-  (config) => {
-    return config;
-  },
+api.interceptors.response.use(
+  (response) => response,
   (error) => {
-    return Promise.reject(error);
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(error);
+    }
   }
 );
 
